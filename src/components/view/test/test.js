4803409e22cc84d9ee347ff5379835aa9f86d9c8
Administrator <1535703141@qq.com>
2018-10-09 22:58:43 +0800

    配置jest测试工具

--- src/components/view/test/test.js
+++
@@ -1,8 +0,0 @@
-import Vue from '../base.js'
-import Test from '$src/components/view/test/test.vue'
-
-let vm=new Vue({
-    el:'#app',
-    components: {Test},
-    template: '<Test/>'
-})
---
+++ test/first.test.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import Component from '../src/components/base/button/ws-base-button-text.vue'
+
+describe('Component', () => {
+    test('是一个 Vue 实例', () => {
+        const wrapper = mount(Component)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+})
