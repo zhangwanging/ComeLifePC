f48fc33a9cac5ac497862d2ee3683ef36c2f1e80
Administrator <1535703141@qq.com>
2018-10-14 16:56:05 +0800

    网络请求API重构前

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue/dist/vue'
 import App from '$src/App.vue'
 import router from '$src/router/index'
